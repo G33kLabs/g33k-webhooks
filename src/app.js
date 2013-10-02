8aa23475d2a2fdbb04b9e1c2366b5208049602c3
G33k <delarueguillaume@gmail.com>
2013-10-02 04:42:39 +0200

    Debug

--- src/app.js
+++ src/app.js
@@ -41,7 +41,7 @@ opts.version = version;
 
 // Setup logging
 winston.remove(winston.transports.Console);
-winston.add(winston.transports.Console, {colorize: true}); 
+winston.add(winston.transports.Console, {colorize: true, level: opts.debug?'debug':null}); 
 
 // Create a gith server on port process.env.PORT
 var gith = require('gith').create(opts.port) ;
